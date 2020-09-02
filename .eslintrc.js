2dafb22354b7dc94abd5d5a063310332d4a93d72
taoguo <807081817@qq.com>
2020-09-02 19:57:51 +0800

    n

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,21 @@
 module.exports = {
   root: true,
-
   env: {
     node: true
   },
-
-  parserOptions: {
-    parser: 'babel-eslint',
-    ecmaVersion: 2020
-  },
-
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-
   extends: [
     'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/standard',
-    'tg-config',
-  ]
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    // 忽略尾逗号检测，随便你写不写
+    'comma-dangle': 'off',
+    'prefer-promise-reject-errors': 'off',
+  }
 }
