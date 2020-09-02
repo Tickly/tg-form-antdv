0726727f17ba7d5c079b0fb25bee9d6f146b4fd5
taoguo <807081817@qq.com>
2020-09-02 19:03:52 +0800

    更新依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,15 +13,12 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/order-in-components': 'error',
-    'no-unused-vars': 'off',
-    // 忽略尾逗号检测，随便你写不写
-    'comma-dangle': 'off'
   },
 
   extends: [
     'plugin:vue/essential',
     'eslint:recommended',
-    '@vue/standard'
+    '@vue/standard',
+    'tg-config',
   ]
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -24,7 +24,7 @@ export default {
   data () {
     return {
       model: null,
-      errors: {}
+      errors: {},
     }
   },
   computed: {
